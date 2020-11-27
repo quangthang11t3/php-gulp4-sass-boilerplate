ba46991a52feebb54caa37f9d22c86ed9ac3ca79
dh_tan <tan@demo.co.jp>
2020-11-27 11:56:56 +0900

    add object fit for IE

--- src/assets/inc/footer.php
+++ src/assets/inc/footer.php
@@ -3,6 +3,7 @@
     </footer><!-- ./footer -->
   </div>
   <script src="assets/js/libs/jquery-3.5.1.min.js"></script>
+  <script src="assets/js/libs/fitie.js"></script>
   <!-- <script src="assets/js/libs/slick.min.js"></script> -->
   <script src="assets/js/common.js"></script>
   <!-- <script src="assets/js/top.js"></script> -->
